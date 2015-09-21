1d78055d774d99b15f911cb97d021769c3af70ff
Summer Brochtrup & Momo Ozawa <student@epicodus.com>
2015-09-21 16:46:10 -0700

    Update image url

--- js/city.js
+++ js/city.js
@@ -4,29 +4,30 @@
 
  var geocoder;
  var map;
- 
 
- function initialize() {
-   geocoder = new google.maps.Geocoder();
-   var latlng = new google.maps.LatLng(-34.397, 150.644);
-   var mapOptions = {
-     zoom: 3,
-     center: latlng
-   }
-   map = new google.maps.Map(document.getElementById("map"), mapOptions);
- }
+
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(-34.397, 150.644);
+  var mapOptions = {
+    zoom: 3,
+    center: latlng
+  }
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
 
  function codeAddress() {
    // Get address from form
    var address = $("input#new-location-name").val();
    geocoder.geocode( { 'address': address}, function(results, status) {
-     if (status == google.maps.GeocoderStatus.OK) {
+    if (status == google.maps.GeocoderStatus.OK) {
       var location = results[0].geometry.location;
       map.setCenter(location);
 
       var formattedAddress = results[0].formatted_address;
+      var placeId = results[0].place_id;
 
-      var contentString = "<div id='content'><h4>" + formattedAddress + "</h4><img class='streetview' src='https://maps.googleapis.com/maps/api/streetview?size=400x400&pano=" + formattedAddress + "&fov=120&heading=235&pitch=10&key=AIzaSyDyoP_3UjpK3YSJT8g6-ngD1WzFv1seqLY'></div>";
+      var contentString = "<div id='content'><h4>" + formattedAddress + "</h4><img class='streetview' src='https://maps.googleapis.com/maps/api/streetview?size=400x400&location=" + formattedAddress + "&fov=120&heading=235&pitch=10&key=AIzaSyDyoP_3UjpK3YSJT8g6-ngD1WzFv1seqLY'></div>";
 
       var infowindow = new google.maps.InfoWindow({
         content: contentString
@@ -34,19 +35,19 @@
 
       // Add marker
       var marker = new google.maps.Marker({
-         map: map,
-         position: results[0].geometry.location
+        map: map,
+        position: results[0].geometry.location
       });
 
       // Add eventlistner to zoom 8x on click
       marker.addListener('click', function() {
-       infowindow.open(map, marker);
+        infowindow.open(map, marker);
       });
-     } else {
+    } else {
        alert("Geocode was not successful for the following reason: " + status);
-     }
-   });
- }
+    }
+  });
+}
 
 
 /**
